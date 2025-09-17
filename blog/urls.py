6adcda780713f8379c0b70dd01b0a9149952c8f7
aparna <pant.aparna87@gmail.com>
2025-09-18 00:43:06 +0100

    Create new homepage, move blog list to /stories/

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.homepage, name = 'homepage'),
+    path('stories/', views.post_list, name= 'post_list'),
     path('post/<int:pk>/delete/', views.post_delete, name = 'post_delete'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/portfolio/', views.portfolio, name='portfolio'),
+    path('portfolio/', views.portfolio, name='portfolio'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,9 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'blog/homepage.html')
+
 def post_list(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
   return render ( request, 'blog/post_list.html', {'posts' : posts})
